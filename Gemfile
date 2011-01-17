4968a02164b3a6faa348ebba660b9c0961c748b7
Fernando Espinosa <fer@ferdev.com>
2011-01-17 23:35:15 +0100

    Add config.ru file and some deployment tweaks

--- Gemfile
+++ Gemfile
@@ -3,7 +3,3 @@ source "http://rubygems.org"
 gem 'sinatra'
 
 gem 'capistrano'
-
-group :production do
-  gem 'passenger'
-end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,14 @@
 require "bundler/capistrano"
 
 set :application, "cartoset"
-set :repository, "git@github.com:Vizzuality/cartoset.git"
 
 set :user,  'ubuntu'
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
 set :scm, :git
+set :repository, "git@github.com:Vizzuality/cartoset.git"
+set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :branch, "production"
 
@@ -16,10 +17,10 @@ set :deploy_to, "/home/ubuntu/www/#{application}"
 role :web, "178.79.142.149"
 role :app, "178.79.142.149"
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
